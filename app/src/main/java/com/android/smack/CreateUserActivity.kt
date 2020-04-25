25f2fef8802bf6b0b6675b9082921011970b46f0
Rohan Wagh <rohan.wagh@cognizant.com>
2020-04-25 18:47:55 +0530

    CreateUserActivity design implemented

---
+++ app/src/main/java/com/android/smack/CreateUserActivity.kt
@@ -0,0 +1,27 @@
+package com.android.smack
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import com.android.smack.R
+
+class CreateUserActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_user)
+    }
+
+
+    fun generateBackgroundColorButtonClicked(view: View){
+
+    }
+
+    fun createUserButtonClicked(view: View){
+
+    }
+
+    fun generateUserAvatar(view: View){
+
+    }
+}
